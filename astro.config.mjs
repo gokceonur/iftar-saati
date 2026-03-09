7b6af478326e45be94972f69577a9c563c53f6d4
Onur Gokce <onurgokcecom@gmail.com>
2026-03-09 19:13:52 +0300

    github action auto upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://iftarsaati.gokceonur.com',
   vite: {
     json: {
       // Import full JSON objects rather than named exports
